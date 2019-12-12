a4e71ca8b23b8c701bfacb9640304b192864063d
sean chang <chang.c.h@hotmail.com>
2019-12-11 22:03:57 -0800

    clean up

--- ToyBox.h
+++ ToyBox.h
@@ -9,12 +9,9 @@ class ToyBox: public plainbox<Itemtype>
 {
     public:
       typedef std::shared_ptr<ToyBox> Ptr;
-
       ToyBox(){};
-
       ToyBox(const Itemtype in_type, const Color in_color)
        :plainbox<Itemtype>(in_type), boxcolor(in_color){};
-
       Color GetColor(){ return boxcolor;}
     private:
      Color boxcolor;
